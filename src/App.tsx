939c064c1135672284fed3fd79e82b62d9a82a27
LinkedWrong <79053675+LinkedWrong@users.noreply.github.com>
2023-01-13 22:59:44 +0300

    add new features

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import { useQRScanner, useWebApp } from "twa-sdk-react"
+import { usePopup } from "twa-sdk-react"
+import { useMainButton } from "twa-sdk-react"
 
 function App() {
+  const webApp = useWebApp()
+  const scanner = useQRScanner()
+  const popup = usePopup()
+  const mainButton = useMainButton()
+  // const [message, _] = useState("")
+
+  useEffect(() => {
+    const openScanner = async () => {
+      let res = await scanner.open()
+      if (res) {
+        await popup.open({ message: res })
+      }
+      scanner.close()
+    }
+
+    mainButton.setText("Сканировать").show()
+    mainButton.on("click", openScanner)
+    mainButton.commit()
+    webApp.ready()
+  }, [webApp, mainButton, popup, scanner])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <p>{message}</p> */}
+      <p>{webApp.platform}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { PropsWithChildren } from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
+import { SDKProvider, useSDK } from "twa-sdk-react"
+import { InitOptions } from "twa-sdk"
+import { stringify } from "querystring"
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+export const Loader = ({ children }: PropsWithChildren) => {
+  const { didInit, components, error } = useSDK()
+
+  if (!didInit) {
+    return <div>SDK init function is not yet called.</div>
+  }
+
+  if (error !== null) {
+    return <div>Something went wrong. {stringify(error as {})}</div>
+  }
+
+  if (components === null) {
+    return <div>Warming up SDK.</div>
+  }
+
+  // Safely render application.
+  return <>{children}</>
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+
+const options: InitOptions = {
+  debug: true,
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <SDKProvider initOptions={options}>
+      <Loader>
+        <App />
+      </Loader>
+    </SDKProvider>
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
